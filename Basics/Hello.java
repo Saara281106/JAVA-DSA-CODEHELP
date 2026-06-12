7813987fe5c3e85beec85e8e48e8951f31c2dec0
Saara <saara.281106@gmail.com>
2026-06-12 16:00:49 +0530

    First code and Variables done

---
+++ Basics/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        //System.out.print(""); This line is used to print.
+        System.out.print("4 + 3 = ");
+        System.out.print(4+3);
+
+    }
+}
---
+++ Basics/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args){
+        //Lets do variables
+        int age = 18;
+        System.out.println(age);
+        int marks;
+        marks = 100;
+        marks = 487;
+        System.out.println(marks);
+        int $jdbvjnn=4292;
+        System.out.println($jdbvjnn);
+
+    }
+}
