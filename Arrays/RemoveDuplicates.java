4aa33e4b9d382d37bd3a0b5daa22988bf11fb097
Saara <saara.281106@gmail.com>
2026-08-19 00:17:27 +0530

    Remove duplicates from array and keep only unique elementrs

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -2,25 +2,40 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         System.out.println();
 
-        int arr[] = { 1, 2, 3, 4, 5, 4, 3 };
+        // Task is to remove duplicates ans return new length without duplicates
+
+        int arr[] = { 1, 2, 3, 3, 3, 4, 4, 5 };
 
         System.out.println("Orinal Array:-");
-        for(int i=0 ; i<arr.length ; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
-        removeDuplicate(arr);
 
         System.out.println();
         System.out.println("Array after removing duplicates:-");
-        for(int i=0 ; i<arr.length ; i++){
-            System.out.print(arr[i] + " ");
-        }
+        removeDuplicate(arr);
 
         System.out.println();
     }
 
-    static void removeDuplicate(int arr[]){
+    static void removeDuplicate(int arr[]) {
         int n = arr.length;
+        int i = 0;
+        int j = 1;
+        while (j < n) {
+            if (arr[i] == arr[j]) {
+                j++;
+            } else {
+                i++;
+                arr[i] = arr[j];
+                j++;
+            }
+        }
+        for (int x = 0; x <= i; x++) {
+            System.out.print(arr[x] + " ");
+        }
+        // for(int x=i+1 ; x<n ; x++){
+        // arr[x] = 0;
+        // }
     }
 }
