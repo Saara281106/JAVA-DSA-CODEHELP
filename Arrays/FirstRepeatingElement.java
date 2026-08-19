858310d6f483df91fda22f5ddc1821108c2ce209
Saara <saara.281106@gmail.com>
2026-08-19 23:15:11 +0530

    Finding first repeating element for unsorted array

--- Arrays/FirstRepeatingElement.java
+++ Arrays/FirstRepeatingElement.java
@@ -2,17 +2,36 @@ public class FirstRepeatingElement {
     public static void main(String[] args) {
         System.out.println();
 
-        int arr[] = { 1, 2, 2, 3, 4, 4, 5, 5, 5, 6 };
-        findFirstRepeating(arr);
+        int arr[] = { 1, 2, 3, 4, 5, 5, 5, 6, 1 };
+        firstRepeating(arr);
 
         System.out.println();
     }
 
-    static void findFirstRepeating(int arr[]){
+    // if the array is sorted
+    static void findFirstRepeating(int arr[]) {
         int n = arr.length;
-        for(int i=0 ; i<n-1 ; i++){
-            if(arr[i] == arr[i+1]){
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
                 System.out.println("The first repeating value is: " + arr[i]);
+                break;
+            }
+        }
+    }
+
+    // if the array is unsorted
+    static void firstRepeating(int arr[]) {
+        int n = arr.length;
+        boolean found = false;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("The first repeating value is: " + arr[j]);
+                    found = true;
+                    break;
+                }
+            }
+            if (found == true) {
                 break;
             }
         }
