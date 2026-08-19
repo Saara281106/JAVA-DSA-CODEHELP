1e9d8e7198a810c1a5e459a8e64938414d4742b7
Saara <saara.281106@gmail.com>
2026-08-20 00:07:12 +0530

    Optimized Solution

--- Arrays/FindPivotIndex.java
+++ Arrays/FindPivotIndex.java
@@ -3,7 +3,7 @@ public class FindPivotIndex {
         System.out.println();
 
         int arr[] = { 1, 7, 3, 6, 5, 7, 10 };
-        int index = pivotIndex(arr);
+        int index = optimizedSol(arr);
         System.out.println("The pivot index is: " + index);
 
         System.out.println();
@@ -22,6 +22,30 @@ public class FindPivotIndex {
                 rightSum += arr[j];
             }
             if (leftSum == rightSum) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    static int optimizedSol(int arr[]) {
+        int n = arr.length;
+        int leftSum[] = new int[n];
+        int rightSum[] = new int[n];
+
+        leftSum[0] = arr[0];
+        for(int i=1 ; i<n ; i++){
+            leftSum[i] = leftSum[i-1] + arr[i];
+        }
+
+        rightSum[n-1] = arr[n-1];
+        for(int i=n-2 ; i>=0 ; i--){
+            rightSum[i] = rightSum[i+1] + arr[i];
+        }
+    
+        for(int i=0 ; i<n ; i++){
+            if(rightSum[i] == leftSum[i]){
                 return i;
             }
         }
