32b44bc5b9be8c1b85ac14115ffc3c110472adb7
Saara <saara.281106@gmail.com>
2026-09-18 11:26:50 +0530

    updated sum of columns of array

--- Arrays/TwoDArray/PrintSumOfColumn.java
+++ Arrays/TwoDArray/PrintSumOfColumn.java
@@ -1,6 +1,6 @@
 public class PrintSumOfColumn {
     public static void main(String[] args) {
-        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int arr[][] = { { 1, 2, 3, 1 }, { 4, 5, 6, 1 }, { 7, 8, 9, 1 } };
         int sum[] = printSumOfRow(arr);
         for (int i : sum) {
             System.out.print(i + " ");
@@ -9,20 +9,21 @@ public class PrintSumOfColumn {
 
     static int[] printSumOfRow(int arr[][]) {
         int n = arr.length;
-        int ans[] = new int[n];
+        int m = arr[0].length;
+
+        //Ans array ki length ab no. of elements in a column jitni hogi 
+        int ans[] = new int[m];
 
         int k = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             int sum = 0;
-            int m = arr[i].length;
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 sum += arr[j][i];
             }
             ans[k] = sum;
             k++;
         }
- 
+
         return ans;
     }
 }
-
