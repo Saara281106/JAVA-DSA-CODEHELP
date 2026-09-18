757b18013c562ccd8e56a607820fd6702af165c5
Saara <saara.281106@gmail.com>
2026-09-18 10:59:15 +0530

    Sum of each row in a 2D array

---
+++ Arrays/TwoDArray/PrintSumOfRow.java
@@ -0,0 +1,27 @@
+public class PrintSumOfRow {
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int sum[] = printSumOfRow(arr);
+        for (int i : sum) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static int[] printSumOfRow(int arr[][]) {
+        int n = arr.length;
+        int ans[] = new int[n];
+
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int m = arr[i].length;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            ans[k] = sum;
+            k++;
+        }
+
+        return ans;
+    }
+}
