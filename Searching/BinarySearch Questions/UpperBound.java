9ebecdbc589f998bec189e35c695443b902ea1d1
Saara <saara.281106@gmail.com>
2026-09-23 23:18:55 +0530

    Upper Bound

---
+++ Searching/BinarySearch Questions/UpperBound.java
@@ -0,0 +1,34 @@
+public class UpperBound {
+    public static void main(String[] args) {
+        System.out.println();
+
+        int arr[] = { -4, -3, 0, 1, 2, 2, 2, 2, 2, 2, 2, 3, 15, 16, 80 };
+        int target = 2;
+        int ans = getUpperBound(arr, target);
+        System.out.println("Upper Bound of " + target + ": " + ans);
+
+        System.out.println();
+        System.out.println();
+    }
+
+    static int getUpperBound(int arr[], int target) {
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int mid = s - ((s - e) / 2);
+        int ans = -1;
+
+        while (s <= e) {
+            if (arr[mid] > target) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+
+            mid = s - ((s - e) / 2);
+        }
+
+        return ans;
+    }
+}
