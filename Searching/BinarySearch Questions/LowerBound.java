e1c3fb990281922f9338cebf4f176922a3555f88
Saara <saara.281106@gmail.com>
2026-09-23 23:05:15 +0530

    Lower Bound Code

---
+++ Searching/BinarySearch Questions/LowerBound.java
@@ -0,0 +1,34 @@
+public class LowerBound {
+    public static void main(String[] args) {
+        System.out.println();
+
+        int arr[] = { -4, -3, 0, 1, 2, 2, 3 };
+        int target = 2;
+        int ans = lowerBound(arr, target);
+        System.out.println("Lower Bound of " + target + ": " + ans);
+
+        System.out.println();
+        System.out.println();
+    }
+
+    static int lowerBound(int arr[], int target) {
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int mid = s - ((s - e) / 2);
+        int ans = -1;
+
+        while (s <= e) {
+            if (arr[mid] >= target) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+
+            mid = s - ((s - e) / 2);
+        }
+
+        return ans;
+    }
+}
---
+++ Searching/BinarySearch Questions/NumberOfOccurrence.java
@@ -0,0 +1,20 @@
+public class NumberOfOccurrence {
+    public static void main(String[] args){
+        System.out.println();
+
+        int arr[] = {1,1,2,2,2,2,3};
+        int target = 2;
+        int ans = numberOfOccurrence(arr , target);
+        System.out.println("Number of occurence of " + target + ": " + ans);
+
+        System.out.println();
+        System.out.println();
+    }
+
+    static int numberOfOccurrence(int arr[] , int target){
+        // int n = arr.length;
+        int count = 0;
+
+        return count;
+    }
+}
